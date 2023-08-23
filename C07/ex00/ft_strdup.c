4d0bdd46906531c7c9131ec0ad910a9db5ec69d8
Antoine Chosson <achosson@bess-f1r2s9.clusters.42paris.fr>
2023-08-23 21:33:04 +0200

    C07 ex00

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achosson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 17:59:36 by achosson          #+#    #+#             */
+/*   Updated: 2023/08/23 20:11:03 by achosson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		l;
+	char	*dest;
+
+	i = 0;
+	l = 0;
+	while (src[l])
+		l++;
+	dest = malloc(sizeof(char) * (l + 1));
+	if (dest == NULL)
+		return (0);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%s", ft_strdup("COUCOU\n"));
+	return (0);
+}
+*/
